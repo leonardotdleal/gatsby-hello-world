6d7a505f4f794a3429fcbe427ff65048cb46bb1e
Leonardo Leal <leonardotade@gmail.com>
2019-09-03 17:26:01 -0300

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
@@ -24,7 +25,8 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div>
+      <Helmet title={data.site.siteMetadata.title} />
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -41,7 +43,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,61 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import "./index.css"
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    {/* <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2</Link> */}
+
+    <div>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.frontmatter.date} className="article-box">
+          <Link
+            to={node.fields.slug}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <h3 className="title">{node.frontmatter.title}</h3>
+          </Link>
+          <p className="author">{node.frontmatter.author}</p>
+          <p className="date">
+            {node.frontmatter.date} {node.timeToRead}min read
+          </p>
+          <p className="excerpt">{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query HomePageQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+            author
+          }
+          excerpt
+          timeToRead
+        }
+      }
+    }
+  }
+`
